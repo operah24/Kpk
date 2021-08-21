2a30516e8e150b1614c75d67a8941199aad95bec
Opeyemi <66219854+operah24@users.noreply.github.com>
2021-08-21 22:44:28 +0100

    initial commmit

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,8 +2,10 @@ import React from 'react';
 import classes from './Landing.module.css'
 const Landing = () => {
   return (
-    <div className={classes.landing}>
+    <div className={classes.container}>
+      <div className={classes.landing}>
       <h1 className = {classes.land}>WELCOME TO HNG.</h1>
+    </div>
     </div>
   );
 }
